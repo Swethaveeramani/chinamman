33b42acf31fbe65ab8cec6836189a82cdf4b7132
swetha@2703 <swethav.21it@kongu.edu>
2024-03-19 21:20:20 +0530

    traders

---
+++ client/src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+  {
+    _id: 0,
+    name: 'Any',
+    array: [],
+  },
+  {
+    _id: 1,
+    name: 'Rs.0 to Rs.500',
+    array: [0, 9],
+  },
+  {
+    _id: 2,
+    name: 'Rs.500 to Rs.1000',
+    array: [10, 19],
+  },
+  {
+    _id: 3,
+    name: 'Rs.1000 to Rs.2000',
+    array: [20, 29],
+  },
+  {
+    _id: 4,
+    name: 'Rs.2000 to Rs.3000',
+    array: [30, 39],
+  },
+  {
+    _id: 5,
+    name: 'More than 3000',
+    array: [40, 99],
+  },
+];
